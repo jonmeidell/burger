a08255b0566e605ccafc8e0e98dfee1ceb2127fe
jonmeidell <jonmeidell@gmail.com>
2020-05-31 22:00:03 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salt City Burger - Garlic Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lucky 13 - Garlic Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Gordon Ramsay Burger - Hells' Kitchen Burger", true);
