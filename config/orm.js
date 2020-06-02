81c848fa1f935439ac53bd16bc679dc16faa8ef9
jonmeidell <jonmeidell@gmail.com>
2020-06-01 20:47:19 -0600

    config, model, controller, view setup

---
+++ config/orm.js
@@ -0,0 +1,92 @@
+// Create an orm.js file inside config directory.
+
+
+// Import (require) connection.js into orm.js
+
+
+// In the orm.js file, create the methods that will execute the
+// necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+// selectAll()
+// insertOne()
+// updateOne()
+
+
+
+// Export the ORM object in module.exports.
+const connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+    let arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
+
+function objToSql(ob) {
+    let arr = [];
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+        let value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+let orm = {
+    selectAll: function (tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    updateOne: function (table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            };
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Salt City Burger - Garlic Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Lucky 13 - Garlic Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Gordon Ramsay Burger - Hells' Kitchen Burger", true);
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salt City Burger - Garlic Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lucky 13 - Garlic Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Gordon Ramsay Burger - Hells' Kitchen Burger", 1);
