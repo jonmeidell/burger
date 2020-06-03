54ef80131994c3171b177eb6c63231a129d06f37
jonmeidell <jonmeidell@gmail.com>
2020-06-02 21:48:09 -0600

    Jaws!

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-	host: "localhost",
-	port: 3308,
-	user: "root",
-	password: "root",
-	database: "burgers_db"
-});
+const connection;
+// JawsDB
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		port: 3307,
+		user: "root",
+		password: "root",
+		database: "burgers_db"
+	});
+};
 // Make connection.
 connection.connect(function (err) {
 	if (err) {
