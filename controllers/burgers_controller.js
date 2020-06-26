88583f7c73ec73e686c5cf7ddee8f079b0c49455
jonmeidell <jonmeidell@gmail.com>
2020-06-25 18:44:47 -0600

    added burgertime image

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,6 @@
 // Inside the burgers_controller.js file, import the following:
-
 // Express
 // burger.js
-
-
 
 // Create the router for the app,
 // and export the router at the end of your file.
@@ -12,8 +9,8 @@ const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger");
 
-router.get("/", function (req,res) {
-    burger.selectAll(function(data){
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         let hbsObject = {
             burgers: data
         };
