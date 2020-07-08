6310aa49fb49758a704afd5027243984b586a73a
jonmeidell <jonmeidell@gmail.com>
2020-07-07 22:00:12 -0600

    minor fixes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require("express");
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static("public"));
