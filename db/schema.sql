b916dd934e1d670ddddd26b7d095848f84452dfb
jonmeidell <jonmeidell@gmail.com>
2020-07-07 22:23:02 -0600

    minor fixes

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers_db.burgers (
+CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(100) NOT NULL,
   devoured BOOLEAN NOT NULL,
