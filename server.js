1c02fc12f86561be5b1481758a78dcaf338f3da3
jonmeidell <jonmeidell@gmail.com>
2020-07-09 18:32:20 -0600

    troubleshooting

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.set("view engine", "handlebars");
 const routes = require("./controllers/burgers_controller");
 app.use(routes);
 app.listen(PORT, function () {
-    console.log("Grilling burgers on http://localhost:" + PORT);
+    console.log("Grilling burgers on " + PORT);
 });
