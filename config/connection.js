6cc351851fe32d94b4e74f8bca6d662e3ae717cd
jonmeidell <jonmeidell@gmail.com>
2020-07-18 11:35:28 -0600

    file clean-up

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,5 @@
 const mysql = require("mysql");
 
-// 	const connection = mysql.createConnection({
-// 		host: "localhost",
-// 		port: 3307,
-// 		user: "root",
-// 		password: "root",
-// 		database: "burgers_db"
-// 	});
-
-// // Make connection.
-// connection.connect(function (err) {
-// 	if (err) {
-// 		console.error("error connecting: " + err.stack);
-// 		return;
-// 	}
-// 	console.log("connected as id " + connection.threadId);
-// });
-// // Export connection for our ORM to use.
-// module.exports = connection;
 if (process.env.NODE_ENV === 'production') {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,3 @@
-// Inside the burgers_controller.js file, import the following:
-// Express
-// burger.js
-
-// Create the router for the app,
-// and export the router at the end of your file.
-
 const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger");
